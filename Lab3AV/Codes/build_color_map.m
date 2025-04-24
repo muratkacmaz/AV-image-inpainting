e38576f4bb2839f8786e9c8232c9bbd0dbdbc62a
muratkacmaz <kacmazmurat18@gmail.com>
2025-04-24 12:37:30 +0200

    init commit

---
+++ Lab3AV/Codes/build_color_map.m
@@ -0,0 +1,22 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%              LABORATORY #3 
+%%%              ARTIFICIAL VISION 2024-2024
+%%%              Exemplar-based methods and applications
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function color_map = build_color_map(size)
+%build_color_map Builds a color map where each pixel has its own color
+%   size - desired size of a color map: [height width]
+
+    color_map = zeros(size(1), size(2), 3);
+    for i=1:size(1)
+        for j=1:size(2)
+            color_map(i, j, 1) = j * 255 / size(2);
+            color_map(i, j, 2) = i * j * 255 / (size(1) * size(2));
+            color_map(i, j, 3) = i * 255 / size(1);
+        end
+    end
+    
+    color_map = uint8(color_map);
+end
+
---
+++ Lab3AV/Codes/compute_patch_distances.m
@@ -0,0 +1,22 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%              LABORATORY #3 
+%%%              ARTIFICIAL VISION 2024-2025
+%%%              Exemplar-based methods and applications
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function distances = compute_patch_distances(patch_list, patch, mask, weights)
+%compute_patch_distances Computes distances between the given patch and 
+%                        all patches in the list of patches
+%   IN:
+%	patch_list - list of N patches in 4D structure:
+%       (patch height, patch width, number of color channels, N)
+%   patch - given patch
+%   mask - binary mask to be applied in distance computation
+%   weights - intra-patch weights
+%   OUT:
+%   distances - array of size N, containing all patch distances
+
+    % TODO: implement
+
+end
+
